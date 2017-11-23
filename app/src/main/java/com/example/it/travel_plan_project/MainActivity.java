ccf478e1b5b4da223b6b77c36dfd6a466180598e
dhukim <dhukim@naver.com>
2017-11-24 04:26:48 +0900

    레이아웃

--- app/src/main/java/com/example/it/travel_plan_project/MainActivity.java
+++ app/src/main/java/com/example/it/travel_plan_project/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.it.travel_plan_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
+        Button login = (Button) findViewById(R.id.login_button);
+        Button sign = (Button) findViewById(R.id.login_sign_button);
 
+        login.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.plan_view);
+            }
+        });
+
+        sign.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.signup);
+            }
+        });
     }
 }
