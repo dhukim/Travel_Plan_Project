ac671192f989fc096c29181cec261211aaf75723
dhukim <dhukim@naver.com>
2017-11-24 04:11:26 +0900

    레이아웃

--- app/src/main/java/com/example/it/travel_plan_project/MainActivity.java
+++ app/src/main/java/com/example/it/travel_plan_project/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.plan_add);
+        setContentView(R.layout.login);
+
+
     }
 }
