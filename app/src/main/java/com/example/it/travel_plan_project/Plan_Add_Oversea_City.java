3d5c60f6fcac49bbff24b92a0785b51f1b0de6d6
dongjuny <dong64553@naver.com>
2017-11-26 22:55:43 +0900

    ?
    ?

---
+++ app/src/main/java/com/example/it/travel_plan_project/Plan_Add_Oversea_City.java
@@ -0,0 +1,8 @@
+package com.example.it.travel_plan_project;
+
+/**
+ * Created by handongjun on 2017. 11. 26..
+ */
+
+public class Plan_Add_Oversea_City {
+}
---
+++ app/src/main/java/com/example/it/travel_plan_project/Plan_Add_Oversea_Country.java
@@ -0,0 +1,18 @@
+package com.example.it.travel_plan_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by handongjun on 2017. 11. 26..
+ */
+
+public class Plan_Add_Oversea_Country extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+    }
+}
